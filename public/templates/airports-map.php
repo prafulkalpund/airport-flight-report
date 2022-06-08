2e3e2487f1d315baad5dca1b7be94666f910cd90
pkalpund79@gmail.com <pkalpund79@gmail.com>
2022-06-08 14:42:05 +0100

    Removing api key

--- public/templates/airports-map.php
+++ public/templates/airports-map.php
@@ -10,15 +10,6 @@ $afs_map_nonce = wp_create_nonce("afs_fetch_all_flights_from_airport_nonce");
     
     <div id="afs_map" style="height: 800px; width: 100%;"></div>
     
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgDWBXUaAtdpQJ8wjRHzQLUFrIaE3RYG0&libraries=places"
+    <script src="https://maps.googleapis.com/maps/api/js?key=ADD_YOUR_KEY&libraries=places"
     defer></script>
 </div>
-
-<!--
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCXPkAsD2V1J8ah8RKorba2MjoR-85cH4k&libraries=places"
-    defer></script>
-    
-    AIzaSyDwYUR5CuOiJqmTRvyCwO6oD7ICQXecSSs
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwYUR5CuOiJqmTRvyCwO6oD7ICQXecSSs&libraries=places"
-    defer></script>
--->
